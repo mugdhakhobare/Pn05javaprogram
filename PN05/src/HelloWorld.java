1c3851b1ed397775759db5012b35363479c2e134
admin <admin@DESKTOP-BT80DKM>
2024-07-11 01:09:37 +0530

    HelloWorld Program

---
+++ PN05/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello Student Welcome To TNS training...");
+
+	}
+
+}
